c90d251c3e7b027fb6f90a321ea0f6a458857677
jarry3369 <mac2009@naver.com>
2019-05-15 12:22:17 +0900

    Add files via upload

---
+++ Home.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-container fluid>
+<div class="text-xs-center">
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="blue lighten-2"
+          dark
+          v-on="on"
+        >
+          Loggggin
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
+          Loggggin
+        </v-card-title>
+
+        <v-card-text>
+          <v-text-field
+            v-model="UserID"
+            label="User ID"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            label="Password"
+            required
+          ></v-text-field>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-btn
+          flat>
+            signup
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            flat
+          >
+            Login
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+        <blockquote>
+          &#8220;First, solve the problem. Then, write the code.&#8221;
+          <footer>
+            <small>
+              <em>&mdash;John Johnson</em>
+            </small>
+          </footer>
+        </blockquote>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
